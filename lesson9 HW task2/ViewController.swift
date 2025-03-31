b8c0df5ca6749ed08f41e5f22e1c8cd4b4455c4d
PetrushenkoDmitry <petrushenkodmitrii@yandex.ru>
2025-03-31 13:35:09 +0300

    Initial Commit

---
+++ lesson9 HW task2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson9 HW task2
+//
+//  Created by Дмитрий Петрушенко on 31/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
